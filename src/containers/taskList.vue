1d92a1839c45caaf4c22d0016fa5374aec2a023d
Aleksei Kostenko <kostenko@woxapp.com>
2019-04-26 14:36:40 +0300

    add components and containers files

---
+++ src/containers/taskList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello Tasks</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <task-list />
+</template>
+
+<script>
+import TaskList from "./../containers/taskList.vue";
+
+export default {
+  components: {
+    TaskList
+  },
+  data() {
+    return {};
+  }
+};
+</script>
