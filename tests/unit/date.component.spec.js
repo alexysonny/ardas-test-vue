5ff9bea692ac731097dd438224df9cc5f9d069ab
Aleksei Kostenko <alexysonny@gmail.com>
2019-04-30 16:00:37 +0300

    start testing components

---
+++ tests/unit/date.component.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils";
+import DateComponent from "./../../src/components/date.component.vue";
+
+describe("Date component test", () => {
+  it("Render default props.", () => {
+    const wrapper = shallowMount(DateComponent);
+    expect(wrapper.props().date).toMatch("");
+  });
+  it("Render passed props", () => {
+    const wrapper = shallowMount(DateComponent);
+    const date = "2015-04-19T06:50:27";
+    expect(wrapper.props().date).toMatch("");
+    const dateString = new Date(date).toDateString();
+    wrapper.setProps({ date });
+    expect(wrapper.vm.getDate).toMatch(dateString);
+  });
+});
---
+++ tests/unit/description.component.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import DescriptionComponent from "./../../src/components/description.component.vue";
+
+describe("Description component test", () => {
+  it("Render default props when has no props", () => {
+    const wrapper = shallowMount(DescriptionComponent);
+    expect(wrapper.props().description).toMatch("");
+  });
+  it("Render passed props", () => {
+    const wrapper = shallowMount(DescriptionComponent);
+    expect(wrapper.props().description).toMatch("");
+    const description = "Some text";
+    wrapper.setProps({ description: description });
+    expect(wrapper.props().description).toMatch(description);
+  });
+});
